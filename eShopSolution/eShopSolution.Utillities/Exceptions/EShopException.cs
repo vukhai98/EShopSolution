93146d9bb8320731d3cd03ec972844b3b02f50a8
ADMIN\Admin <vukhai27091998@gmail.com>
2022-02-19 14:05:30 +0700

    Update product method

--- eShopSolution/eShopSolution.Utillities/Exceptions/EShopException.cs
+++ eShopSolution/eShopSolution.Utillities/Exceptions/EShopException.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eShopSolution.Utillities.Exceptions
 {
-    class EShopException
+    public class EShopException : Exception
     {
+        public EShopException()
+        {
+        }
+
+        public EShopException(string message) : base(message)
+        {
+        }
+
+        public EShopException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+       
     }
 }
