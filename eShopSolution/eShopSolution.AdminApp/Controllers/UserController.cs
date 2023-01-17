1d8734d74b0d90bf7605e84c5f7229dea291a23b
vukhai98 <vukhai27091998@gmail.com>
2023-01-18 00:40:07 +0700

    Fix : Register and add comment code

--- eShopSolution/eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution/eShopSolution.AdminApp/Controllers/UserController.cs
@@ -17,30 +17,44 @@ using System.Threading.Tasks;
 
 namespace eShopSolution.AdminApp.Controllers
 {
-
     public class UserController : Controller
     {
         private readonly IUserApiClient _userApiClient;
 
-        private readonly IConfiguration _configuration;
-    
+        private readonly IConfiguration _configuration;   
 
         public UserController(IUserApiClient userApiClient,IConfiguration configuration)
         {
             _userApiClient = userApiClient;
             _configuration = configuration; 
         }
+
+        /// <summary>
+        ///  View trang index
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// View login
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> Login()
         {
+            // Nếu đăng nhập rồi mà chưa logout mà vào trang login thì phải login ra
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return View();
         }
 
+        /// <summary>
+        /// Phương thức login của client gọi
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> Login(LoginRequest request)
         {
@@ -48,26 +62,44 @@ namespace eShopSolution.AdminApp.Controllers
             {
                 return View(ModelState);
             }
+            // Token được trả về sau khi gọi API
             var token = await _userApiClient.Authenticate(request);
 
+            // Giải mã tooken
             var userPrincipal = this.ValidateToken(token);
+
+            // Cài đặt tham số cho cookie
             var authProperties = new AuthenticationProperties
             {
                 ExpiresUtc = DateTime.UtcNow.AddMinutes(10),
                 IsPersistent = false,                  
             };
+
+            // Gọi HttpContext để login
             await HttpContext.SignInAsync(
                                 CookieAuthenticationDefaults.AuthenticationScheme,
                                 userPrincipal,
                                 authProperties);
+
+            // Login thành công thì redirect về trang chủ
             return RedirectToAction("Index", "Home");
         }
-
+        /// <summary>
+        /// Phương thức logout của client vì logout nên ko cần view
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login", "User");
         }
+
+        /// <summary>
+        /// Hàm để giải mã tooken
+        /// </summary>
+        /// <param name="jwtToken"></param>
+        /// <returns></returns>
         private ClaimsPrincipal ValidateToken(string jwtToken)
         {
             IdentityModelEventSource.ShowPII = true;
--- eShopSolution/eShopSolution.AdminApp/Program.cs
+++ eShopSolution/eShopSolution.AdminApp/Program.cs
@@ -1,12 +1,13 @@
-using eShopSolution.AdminApp.Services;
+﻿using eShopSolution.AdminApp.Services;
 using eShopSolution.ViewModels.System.Users;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// Inject để tạo kết nối tới back-end
 builder.Services.AddHttpClient();
 
+// Nếu chưa login thì redirect về trang login
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
@@ -14,14 +15,16 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.AccessDeniedPath = "/User/Forbidden/";
     });
 
-
+// Inject IUserApiClient vào để sử dụng khi cần
 builder.Services.AddTransient<IUserApiClient,UserApiClient> ();
 
 builder.Services.AddControllersWithViews()
-                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginResquestValidator>()); ;
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginResquestValidator>()); 
+
 IMvcBuilder builder1 = builder.Services.AddRazorPages();
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT");
 #if DEBUG
+// If environment is develop auto reload page razor
 if (environment == Environments.Development)
 {
     builder1.AddRazorRuntimeCompilation();
