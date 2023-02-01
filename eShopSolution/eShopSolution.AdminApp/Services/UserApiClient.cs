822266896f50331f6213e8de96e8480af1e36d99
vukhai98 <vukhai27091998@gmail.com>
2023-02-02 00:44:51 +0700

    Edit get user pagging

--- eShopSolution/eShopSolution.AdminApp/Services/UserApiClient.cs
+++ eShopSolution/eShopSolution.AdminApp/Services/UserApiClient.cs
@@ -1,5 +1,8 @@
-﻿using eShopSolution.ViewModels.System.Users;
+﻿using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace eShopSolution.AdminApp.Services
@@ -7,8 +10,15 @@ namespace eShopSolution.AdminApp.Services
     public class UserApiClient : IUserApiClient
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public UserApiClient(IHttpClientFactory httpClientFactory)
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserApiClient(IHttpClientFactory httpClientFactory,
+                   IHttpContextAccessor httpContextAccessor,
+                    IConfiguration configuration)
         {
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
             _httpClientFactory = httpClientFactory;
         }
         public async Task<string> Authenticate(LoginRequest request)
@@ -16,10 +26,25 @@ namespace eShopSolution.AdminApp.Services
             var json = JsonConvert.SerializeObject(request);
             var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
             var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri("https://localhost:5001");
+            client.BaseAddress = new Uri(_configuration["BaseAddress"]);
             var response = await client.PostAsync("/api/user/authenticate", httpContent);
             var token = await response.Content.ReadAsStringAsync();
             return token;
+        }
+
+        public async Task<PagedResult<UserViewModel>> GetUsersPagings(GetUserPagingRequest request)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var sessions = _httpContextAccessor.HttpContext.Session.GetString("Token");
+            //client.BaseAddress = new Uri(_configuration["BaseAddress"]);
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessions);
+            //var response = await client.GetAsync($"/api/users/paging?pageIndex=" +
+            //    $"{request.PageIndex}&pageSize={request.PageSize}&keyword={request.Keyword}");
+            var response = await client.GetAsync("https://localhost:5001/api/User/paging?BearerToken="+$"{sessions}" +  "&PageIndex=1&PageSize=10");
+
+            var body = await response.Content.ReadAsStringAsync();
+            var users = JsonConvert.DeserializeObject<PagedResult<UserViewModel>>(body);
+            return users;
         }
     }
 }
