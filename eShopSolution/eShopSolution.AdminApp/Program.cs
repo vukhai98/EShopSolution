f74d8de01f64effac49347f1507f99b4eb14f365
vukhai98 <vukhai27091998@gmail.com>
2023-02-12 23:17:34 +0700

    Get list product in web Admin

--- eShopSolution/eShopSolution.AdminApp/Program.cs
+++ eShopSolution/eShopSolution.AdminApp/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddTransient<IUserApiClient, UserApiClient>();
 builder.Services.AddTransient<IRoleApiClient, RoleApiClient>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddSingleton<ILanguageApiClient, LanguageApiClient>();
+builder.Services.AddSingleton<IProductApiClient, ProductApiClient>();
 
 builder.Services.AddControllersWithViews()
                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginResquestValidator>()); ;
